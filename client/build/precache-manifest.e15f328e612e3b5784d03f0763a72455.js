df2175afd4020f7488f79fefcd0d295a7790b56a
AviadC31 <aviadcoh1@gmail.com>
2021-02-13 20:45:08 +0200

    build ready for prod

---
+++ client/build/precache-manifest.e15f328e612e3b5784d03f0763a72455.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef6100a429a7516395ddbfdaf6f10c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf88e69c6349e882662e",
+    "url": "/static/css/main.0cae3c6b.chunk.css"
+  },
+  {
+    "revision": "f9c0496cedc1242ca4ce",
+    "url": "/static/js/2.b6c97361.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b6c97361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf88e69c6349e882662e",
+    "url": "/static/js/main.ba7c0b9d.chunk.js"
+  },
+  {
+    "revision": "8e704cb0a4319d5f1d37",
+    "url": "/static/js/runtime-main.83a940af.js"
+  },
+  {
+    "revision": "eb9536a053468f1cbfab8e33ff25a82a",
+    "url": "/static/media/Favourite.eb9536a0.png"
+  },
+  {
+    "revision": "23d919aed562052a3766a158a8443331",
+    "url": "/static/media/nasa.23d919ae.png"
+  }
+]);
